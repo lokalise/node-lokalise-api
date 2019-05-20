4c20454a29ef1c440926f2902c80f973a034cc50
Marissa Biesecker <marissa.biesecker@gmail.com>
2019-05-20 16:14:59 +0100

    Fix snapshot elementClass TypeError on snapshots

--- dist/collections/snapshots.js
+++ dist/collections/snapshots.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const base_collection_1 = require("./base_collection");
+const snapshot_1 = require("../models/snapshot");
 class Snapshots extends base_collection_1.BaseCollection {
     restore(params) {
         return this.createPromise('POST', params, this.returnBareJSON, this.handleReject, {});
@@ -8,5 +9,6 @@ class Snapshots extends base_collection_1.BaseCollection {
 }
 Snapshots.rootElementName = 'snapshot';
 Snapshots.prefixURI = 'projects/{!:project_id}/snapshots/{:id}';
+Snapshots.elementClass = snapshot_1.Snapshot;
 exports.Snapshots = Snapshots;
 //# sourceMappingURL=snapshots.js.map
