2c1b41c59cc006b2a9c7b11d0248a1b876be0cb9
bodrovis <bodrovis@protonmail.com>
2019-06-07 20:21:41 +0300

    Version 0.0.9

--- dist/collections/translations.js
+++ dist/collections/translations.js
@@ -1,9 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const base_collection_1 = require("./base_collection");
+const translation_1 = require("../models/translation");
 class Translations extends base_collection_1.BaseCollection {
 }
 Translations.rootElementName = 'translations';
 Translations.prefixURI = 'projects/{!:project_id}/translations/{:id}';
+Translations.elementClass = translation_1.Translation;
 exports.Translations = Translations;
 //# sourceMappingURL=translations.js.map
--- dist/models/translation.js
+++ dist/models/translation.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const base_model_1 = require("./base_model");
-class Translations extends base_model_1.BaseModel {
+class Translation extends base_model_1.BaseModel {
 }
-exports.Translations = Translations;
+exports.Translation = Translation;
 //# sourceMappingURL=translation.js.map
