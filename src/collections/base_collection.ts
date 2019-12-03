8bdaf9a329da52fdf7f16ad92515e49c21a80631
Yar Kravtsov <yarlson@gmail.com>
2019-12-03 15:33:20 +0200

    Added promise reject with ApiError

--- src/collections/base_collection.ts
+++ src/collections/base_collection.ts
@@ -1,5 +1,6 @@
 import { ApiRequest } from '../http_client/base';
 import { StandartParams } from '../interfaces/standart_params';
+import { ApiError } from '../models/api_error';
 
 export class BaseCollection {
   protected static rootElementName: string = '';
@@ -54,13 +55,16 @@ export class BaseCollection {
     return arr;
   }
 
-  protected returnBareJSON(json: any) : any {
+  protected populateApiErrorFromJson(json: Object): ApiError {
+    return <ApiError>json;
+  }
+
+  protected returnBareJSON(json: any): any {
     return json;
   }
 
-  protected handleReject(data: any) {
-    console.log(data);
-    return;
+  protected handleReject(data: any): ApiError {
+    return this.populateApiErrorFromJson(data);
   }
 
   protected createPromise(method: any, params: any, resolveFn: any, rejectFn = this.handleReject, body: any = null, uri: any = null) : Promise<any> {
