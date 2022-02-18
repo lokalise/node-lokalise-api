0c2fb5d67ad21dbdacaa044604f76795665fd40d
Carsten Daurehøj <arelstone@gmail.com>
2022-02-18 08:57:34 +0100

    Add better types for Project

--- index.d.ts
+++ index.d.ts
@@ -173,8 +173,50 @@ declare module "@lokalise/node-api" {
     team_id: number;
     base_language_id: number;
     base_language_iso: string;
-    settings: object;
-    statistics: object;
+    settings: {
+      per_platform_key_names: boolean;
+      reviewing: boolean;
+      upvoting: boolean;
+      auto_toggle_unverified: boolean;
+      offline_translation: boolean;
+      key_editing: boolean;
+      inline_machine_translations: boolean;
+      branching: boolean;
+      segmentation: boolean;
+      custom_translation_statuses: boolean;
+      custom_translation_statuses_allow_multiple: boolean;
+    };
+    statistics: {
+      progress_total: number;
+      keys_total: number;
+      team: number;
+      base_words: number;
+      qa_issues_total: number;
+      qa_issues: {
+        not_reviewed: number;
+        unverified: number;
+        spelling_grammar: number;
+        inconsistent_placeholders: number;
+        inconsistent_html: number;
+        different_number_of_urls: number;
+        different_urls: number;
+        leading_whitespace: number;
+        trailing_whitespace: number;
+        different_number_of_email_address: number;
+        different_email_address: number;
+        different_brackets: number;
+        different_numbers: number;
+        double_space: number;
+        special_placeholder: number;
+        unbalanced_brackets: number;
+      };
+      languages: Array<{
+        language_id: number;
+        language_iso: string;
+        progress: number;
+        words_to_do: number;
+      }>;
+    };
   }
 
   export interface QueuedProcess {
