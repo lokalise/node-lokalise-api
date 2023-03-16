1a402d1411b5259bd0e440b5f60c0bd5c19a7125
bodrovis <bodrovis@protonmail.com>
2023-03-16 20:36:08 +0200

    Version 8.4.0

--- src/types/update_key_data.ts
+++ src/types/update_key_data.ts
@@ -2,10 +2,7 @@ import { CreateKeyData } from "./create_key_data";
 import { Filenames as Keynames } from "../types/filenames";
 import { SupportedPlatforms } from "../types/supported_platforms";
 
-export type UpdateKeyData = Omit<
-  CreateKeyData,
-  "key_name" | "translations" | "platforms"
-> & {
+export type UpdateKeyData = Omit<CreateKeyData, "key_name" | "platforms"> & {
   key_name?: string | Keynames;
   merge_tags?: boolean;
   platforms?: SupportedPlatforms[];
