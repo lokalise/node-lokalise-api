13edb8e8f5953f87955778de1c9539e489cc260d
ZoidC <b.dancot@gmail.com>
2023-03-16 19:24:41 +0100

    Fix type UpdateKeyData [fix-type-update_key_data] (#364)

--- src/types/update_key_data.ts
+++ src/types/update_key_data.ts
@@ -2,10 +2,7 @@ import { CreateKeyData } from "./create_key_data.js";
 import { Filenames as Keynames } from "../types/filenames.js";
 import { SupportedPlatforms } from "../types/supported_platforms.js";
 
-export type UpdateKeyData = Omit<
-  CreateKeyData,
-  "key_name" | "translations" | "platforms"
-> & {
+export type UpdateKeyData = Omit<CreateKeyData, "key_name" | "platforms"> & {
   key_name?: string | Keynames;
   merge_tags?: boolean;
   platforms?: SupportedPlatforms[];
