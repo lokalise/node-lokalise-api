1df6a1901c27ac47c13e040e717c6cd657531c35
bodrovis <bodrovis@protonmail.com>
2024-04-24 14:50:24 +0300

    update docs [skip ci]

---
+++ dist/types/webhook_events/project_translations_proofread.d.ts
@@ -0,0 +1,31 @@
+import type { Filenames } from "../filenames.js";
+export type WebhookProjectTranslationsProofread = {
+    event: "project.translations.proofread";
+    action: string;
+    translations: Array<{
+        id: number;
+        value: string;
+        is_proofread: boolean;
+        language: {
+            id: number;
+            iso: string;
+            name: string;
+        };
+        key: {
+            id: number;
+            name: string;
+            filenames: Filenames;
+        };
+    }>;
+    project: {
+        id: string;
+        name: string;
+        branch?: string;
+    };
+    user: {
+        email: string;
+        full_name: string;
+    };
+    created_at: string;
+    created_at_timestamp: number;
+};
